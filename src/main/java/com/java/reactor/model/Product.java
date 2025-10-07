1cc1cd5e24185d02d829419c72c39aaa852eff33
Nalla Sriram <nallas.sriram@gmail.com>
2025-10-07 18:18:04 +0530

    Create Product.java

---
+++ src/main/java/com/java/reactor/model/Product.java
@@ -0,0 +1,16 @@
+package com.java.reactor.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private int id;
+    private String title;
+    private float price;
+    private String description;
+    private String category;
+    private String image;
+    private Rating rating;
+
+}
